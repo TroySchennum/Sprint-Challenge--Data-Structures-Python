3ad89934974ff4440f58c7f0e6d2fceae140bef3
Troy Schennum <tj.schennum@gmail.com>
2020-08-14 17:00:16 -0500

    close to finished sprint

--- names/names.py
+++ names/names.py
@@ -13,10 +13,65 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # check if the value is less than the current node's value 
+        if value < self.value:
+            # does the current node have a left child?
+            if self.left:
+                self.left.insert(value)
+            # otherwise, it doesn't have a left child
+            # we can park the new node here 
+            else:
+                self.left = BSTNode(value)
+        # otherwise the value is greater or equal to the current node's value 
+        else:
+            # does the current node have a right child?
+            if self.right:
+                # if it does, call the right child's `insert` method to repeat the process
+                self.right.insert(value)
+            # otherwise, it doesn't have a right child 
+            # we can park the new node here 
+            else:
+                self.right = BSTNode(value)
+    def contains(self, target):
+        # base case: check self's value to see if it matches the target's 
+        if self.value == target:
+            duplicates.append(target)
+        # otherwise, we need to go either left or right 
+        # compare the target against self's value 
+        if target < self.value:
+            # base case: if there's no node here, then the target is not in the tree
+            if not self.left:
+                return False
+            # otherwise, there is a node there
+            else:
+                # call `contains` on the left child
+               return self.left.contains(target) 
+        else:
+            # base case: if there's no node here, then the target is not in the tree
+            if not self.right:
+                return False
+            # otherwise there's a node there 
+            else:
+                # call `contains` on the right child 
+                return self.right.contains(target)
+                
+for i in names_1 and names_2:
+    bst = BSTNode("troy")
+    bst.insert(i)
+    bst.contains(i)
+
+
+# for i in bst:
+#     if i == i:
+#         duplicates.append(i)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # self.storage = []
+        # self.size = 0
+        self.capacity = capacity
+        
+        self.data = []
 
     def append(self, item):
-        pass
+        
+        self.data.append(item)
+        if len(self.data) == (self.capacity + 1):
+            self.data.pop(0)
+        # increment_address_one = (address + 1) % Length   
+        # self.storage.append(item)
+        # self.size += 1
+        
+
+        # if self.storage == (self.capacity - 1):
+        #     self.size -= 1
+        #     self.storage.pop()
+            
 
     def get(self):
-        pass
+        # return self.storage
+        return self.data
+    
